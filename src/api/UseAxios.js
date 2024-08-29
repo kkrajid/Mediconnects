8125e6b5b360dbcc5da06205491e13653dcb7a1b
kkrajid <kkrajid@gmail.com>
2024-08-29 07:22:35 +0530

    api

--- src/api/UseAxios.js
+++ src/api/UseAxios.js
@@ -5,8 +5,8 @@ import jwt_decode from "jwt-decode";
 
 // export const baseURL = "http://localhost:8000/api/";
 // export const websocketbaseUrl = "127.0.0.1:8000"
-export const baseURL = "https://health.medcare.site/api/";
-export const websocketbaseUrl = "health.medcare.site"
+export const baseURL = "https://mediconnects.site/api/";
+export const websocketbaseUrl = "mediconnects.site"
 
 // export const baseURL = "https://hospital-backend-6tun.onrender.com/api/";
 // export const websocketbaseUrl = "hospital-backend-6tun.onrender.com"
